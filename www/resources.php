7ecef6674345a7657884262f88a0998fd966ab2f
Chris Cook <cook678@gmail.com>
2019-04-28 21:43:43 +0100

    missing file

---
+++ www/resources.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html lang=en>
+
+<head>
+<title>Resources and Acknowledgements | The Beach Boys Cover Database</title>
+<link rel="stylesheet" href="resources/bb.css">
+<meta name="author" content="Chris Cook">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+
+
+</head>
+
+<body id="page">
+<div id="margins" align="center">
+
+<?php
+require_once ( "../../../resources/autoloader.php");
+
+//Autoload classes
+(new autoloader());
+
+echo loadHeader();
+?>
+
+	<div class="mainPageBox">
+		<h2>Resources & Acknowledgements</h2>
+		<p>There are many useful Beach Boys reference sites out their - including Andrew Doe's <a href="http://esquarterly.com/bellagio/index.html">Bellagio</a> site and <a href="https://beachboyslegacy.com">Beach Boys Legacy</a>.
+			<br /><br /> Three forums may be of interest - <a href="http://forums.stevehoffman.tv/forums/music-corner.2/">Steve Hoffman's</a> forum for more general rock chat, and the <a href="http://smileysmile.net/board/index.php/board,2.0.html">Smiley Smile</a> and <a href="http://endlessharmony.boards.net">Endless Harmony</a> forums for Beach Boys chat.
+			<br /><br />Also useful more generally was <a href="https://en.wikipedia.org/wiki/Main_Page">Wikipedia</a> and <a href="discogs.com">Discogs</a>, so thanks to anyone who contributes to those sites.
+			<br /><br />Special thanks to Dogear for his <a href="http://www.45cat.com/45_list_view_record.php?li=1972">list on 45cat</a> and JK for constantly finding interesting non-English covers.
+			<br /><br />And finally, thanks to those who have messaged me with covers to add.
+			<br /><br />A large amount of this code-base can be found on <a href="https://github.com/chriscook18/BBCDB">Github</a>.
+			<br/><br />Code-wise, this site makes use of <a href="https://github.com/joequery/Stupid-Table-Plugin">StupidTable</a> and some responsive table features was based around <a href="https://www.smashingmagazine.com/2019/01/table-design-patterns-web">Smashing Magazine's</a> and <a href="http://adrianroselli.com/2017/11/a-responsive-accessible-table.html">Adrian Roselli's</a> articles on the matter.
+		</p>
+	</div>
+
+</div>
+</body>
+
+
+</html>
